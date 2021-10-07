ce5242489ec3abe5e3f4f89813ce6ba1c2ab0147
Raymond-Iacobacci <66854622+Raymond-Iacobacci@users.noreply.github.com>
2021-10-07 14:42:28 -0700

    Update worker.js

--- frontend/worker.js
+++ frontend/worker.js
@@ -1,1 +1,47 @@
 
+function newUser() {
+  var email = document.getElementById("Email").value;
+  var password = document.getElementById("Username").value;
+  var req = new XMLHttpRequest();
+  req.open("POST", "http://54.183.187.3");
+  req.send();
+}
+/*
+var req = new XMLHttpRequest();
+req.timeout = 800;
+
+req.onreadystatechange = function() {
+    console.log("onreadystatechange");
+    console.log("ReadyState: " + req.readyState);
+    console.log("Response status: " + req.status);
+}
+req.onerror = function(error) {
+    console.log("onerror: " + error);
+}
+req.onabort = function() {
+    console.log("onabort");
+}
+req.ontimeout = function() {
+    console.log("ontimeout");
+}
+
+req.open("GET", "http://192.168.1.36&quot;);
+req.send();
+
+
+
+//////#
+
+fetch("https://ipinfo.io/json")
+
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (myJson) {
+    console.log(myJson.ip);
+  })
+  .catch(function (error) {
+    console.log("Error: " + error);
+  });
+  
+*/
